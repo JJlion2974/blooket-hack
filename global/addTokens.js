d284a24f4b772afb43af467be76835fa2e1156e2
JJlion2974 <98432512+JJlion2974@users.noreply.github.com>
2022-01-25 18:42:06 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -13,10 +13,10 @@ async function getName() {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (100,000,000,000,000 daily)'));
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+    if (tokens > 100,000,000,000,000) {
+        alert('You can only add up to 100,000,000,000,000 tokens daily.');
     };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -28,7 +28,7 @@ async function addCurrencies() {
         credentials: "include",
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 100,000,000,000,000,,
             name: await getName()
         })
     });
